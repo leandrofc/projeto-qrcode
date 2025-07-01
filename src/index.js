f0f95dc1844bd01be47d90bae2c62cdcde8e4f2e
Leandro Fonseca <leandromkt@outlook.com>
2025-06-30 22:03:41 -0300

    main prompt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import prompt from "prompt";
 
-async function main() {}
+import mainPrompt from "./prompts/prompt-main.js";
+
+async function main() {
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) console.log("Escolheu o qrCODE");
+        if (choose.select == 2) console.log("Escolheu o password");
+    });
+
+    prompt.start();
+}
 
 main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -2,8 +2,10 @@ const mainPrompt = [
     {
         name: "select",
         description: "Escolha a ferramenta (1 - QRCODE ou (2 - PASSWORD",
-        pattern: /ˆ[1-2]+$/,
+        pattern: /^[1-2]+$/,
         message: "Escolha apenas entre 1 e 2",
         required: true,
     }
 ]
+
+export default mainPrompt;
